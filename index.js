0b66272b3d7e6b9820dfd2d92fe661d2ef4db56f
admirableyetti <ech.emmanuel.ch@gmail.com>
2023-04-09 15:31:08 -0500

    v1

---
+++ index.js
@@ -0,0 +1,159 @@
+#!/usr//bin/env node
+
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+
+
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+const createClassName = (className) => className.split('_').map((e) => `${e.charAt(0).toUpperCase() + e.slice(1)}`).join('');
+
+
+const classCostructor = {
+  className: null,
+  jsonObject: null,
+  immutable: null,
+}
+
+
+async function init() {
+  const answersClassName = await askClassName()
+  const answersMakeImmutable = await askImmutableClass()
+  const answersJsonObject = await askJson()
+
+  classCostructor.className = answersClassName.class_name
+  classCostructor.jsonObject = answersJsonObject.json_object
+  classCostructor.immutable = answersMakeImmutable.immutable
+
+  await fabricClass({ jsonObject: classCostructor.jsonObject, className: classCostructor.className, immutable: classCostructor.immutable })
+
+}
+
+async function askClassName() {
+  return await inquirer.prompt({
+    name: 'class_name',
+    type: "input",
+    message: 'enter the class name separated by _',
+  })
+}
+async function askImmutableClass() {
+  return await inquirer.prompt({
+    name: 'immutable',
+    type: "confirm",
+    message: 'allow the class to be immuatble (freeze)',
+    default: false,
+  })
+}
+
+
+async function askJson() {
+  return await inquirer.prompt({
+    name: 'json_object',
+    type: "input",
+    message: 'Enter the json object',
+  })
+}
+
+async function fabricClass({ className, jsonObject, immutable } = {}) {
+  const spinner = createSpinner('Creating class...\n').start()
+
+
+  let objectKeys
+  try {
+    const rawJson = await JSON.parse(jsonObject)
+    objectKeys = Object.keys(rawJson)
+  } catch (error) {
+    console.error('Json format incorrect');
+    console.error(error);
+    process.exit(1)
+  }
+
+
+  const classNameBuild = createClassName(className)
+  const constructorKeys = objectKeys.map((e) => `${e} = NULL`).join(',\n');
+  const constructorThisKeys = objectKeys.map((e) => `this.${e} = isNull(${e}) ? null : ${e}`).join(';\n');
+  const classCopyWithKeyValue = objectKeys.map((e) => `${e}: isNull(${e}) ? this.${e} : ${e}`).join(',\n');
+
+  console.log(`'use strict'`);
+
+  console.log(`  
+  class Null {
+    constructor() {
+      Object.seal(this)
+    }
+  }
+
+  const NULL = new Null()
+
+  const isNull = (param) => param instanceof Null
+
+  `);
+
+  console.log(`
+    class ${classNameBuild} {
+      constructor({
+        ${constructorKeys}
+      } = {}) {
+        ${constructorThisKeys}
+        ${immutable ? 'Object.seal(this)' : ''}
+      }
+
+      copyWith({ 
+        ${constructorKeys}
+       } = {}) {
+        return new ${classNameBuild}({
+          ${classCopyWithKeyValue}
+        })
+      }
+    }
+  `);
+
+  spinner.success({ text: 'Done' })
+}
+
+await init()
+
+
+function template() {
+  return `
+  class ${classNameBuild} {
+    constructor({
+      ${constructorKeys}
+    } = {}) {
+      ${constructorThisKeys}
+      Object.freeze(this)
+    }
+
+    copyWith({ 
+      ${constructorKeys}
+     } = {}) {
+      return new ${classNameBuild}({
+        ${classCopyWithKeyValue}
+      })
+    }
+  }
+  `
+}
+
+// class Person {
+//   constructor({
+//     age = NULL,
+//     firstName = NULL,
+//     lastName = NULL,
+//     email = NULL,
+//   } = {}) {
+//     this.age = isNull(age) ? null : age;
+//     this.firstName = isNull(firstName) ? null : firstName;
+//     this.lastName = isNull(lastName) ? null : lastName;
+//     this.email = isNull(email) ? null : email;
+//     Object.freeze(this)
+//   }
+
+//   copyWith({ age = NULL, firstName = NULL } = {}) {
+//     return new Person({
+//       age: isNull(age) ? this.age : age,
+//       firstName: isNull(firstName) ? this.firstName : age
+//     })
+//   }
+// }
